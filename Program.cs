a1741b5590a45339231bc211c88f200efb08b04d
Garrett Chin <garrettchin00@gmail.com>
2026-01-02 15:40:21 -0800

    New addition in C# program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// declare a (string) variable
+String myFriendsName;
+// assign a value to the myFriendsName variable
+myFriendsName = "Alice";
+// use the variable
+Console.WriteLine(myFriendsName);
+Console.ReadKey();
