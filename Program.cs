7f481d6429e0c487f47c52db8866e24b036e52aa
Garrett Chin <garrettchin00@gmail.com>
2026-01-02 15:42:35 -0800

    Added spaces in C# program

--- Program.cs
+++ Program.cs
@@ -3,8 +3,10 @@ Console.WriteLine("Hello, World!");
 
 // declare a (string) variable
 String myFriendsName;
+
 // assign a value to the myFriendsName variable
 myFriendsName = "Alice";
+
 // use the variable
 Console.WriteLine(myFriendsName);
 Console.ReadKey();
